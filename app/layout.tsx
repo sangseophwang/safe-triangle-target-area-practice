7e76b9e3aa18fb82fba9d0ba20d0b64262b6be0b
SangSeop Hwang <sshwang12345@gmail.com>
2024-08-01 19:22:20 +0900

    feat: apply common body style

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { PropsWithChildren } from 'react';
 
 import { siteMeta } from '@/policy/meta';
+import CommonStyle from '@/styles/layout/Common.module.scss';
 
 import '@/styles/styles.scss';
 
@@ -10,7 +11,7 @@ export const metadata: Metadata = siteMeta;
 const RootLayout = ({ children }: PropsWithChildren) => {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body className={CommonStyle.body}>{children}</body>
     </html>
   );
 };
