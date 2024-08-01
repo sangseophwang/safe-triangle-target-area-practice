9e06b697e5763821cb93105018b0e999b2e81cb7
SangSeop Hwang <sshwang12345@gmail.com>
2024-08-01 19:20:32 +0900

    misc: init next config

---
+++ next.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: {
+        loader: '@svgr/webpack',
+        options: {
+          svgoConfig: {
+            plugins: [
+              {
+                name: 'removeViewBox',
+                active: false,
+              },
+            ],
+          },
+        },
+      },
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
