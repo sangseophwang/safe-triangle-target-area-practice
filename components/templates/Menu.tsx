4aa50cd91156ea1cc5dfbd461d2b27fbf101dec2
SangSeop Hwang <sshwang12345@gmail.com>
2024-08-14 15:33:29 +0900

    feat: apply useMemo hook at Menu components

--- components/templates/Menu.tsx
+++ components/templates/Menu.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { ForwardedRef } from 'react';
+import { ForwardedRef, MouseEventHandler } from 'react';
 
 import clsx from 'clsx';
 
@@ -8,18 +8,19 @@ import MenuStyle from '@/components/templates/styles/Menu.module.scss';
 import { TypeMenu } from '@/types/menu';
 
 interface TypeMenuList {
+  list: Array<TypeMenu>;
   className?: string;
   menuRef?: ForwardedRef<HTMLMenuElement>;
   menuItemRef?: ForwardedRef<HTMLLIElement>;
-  list: Array<TypeMenu>;
+  updateCursorLocation?: MouseEventHandler<HTMLButtonElement>;
 }
 
-const Menu = ({ className, list, menuRef, menuItemRef }: TypeMenuList) => {
+const Menu = ({ className, list, menuRef, menuItemRef, updateCursorLocation }: TypeMenuList) => {
   return (
     <menu ref={menuRef} className={clsx(MenuStyle.container, className)}>
       {list.map((item, index) => (
         <li ref={menuItemRef} key={index} className={MenuStyle.item}>
-          <MenuItem {...item} />
+          <MenuItem {...item} updateCursorLocation={updateCursorLocation} />
         </li>
       ))}
     </menu>
--- components/templates/MenuItem.tsx
+++ components/templates/MenuItem.tsx
@@ -6,10 +6,17 @@ import Menu from '@/components/templates/Menu';
 import MenuItemStyle from '@/components/templates/styles/MenuItem.module.scss';
 import { TypeMenu } from '@/types/menu';
 
-const MenuItem = ({ title, items }: TypeMenu) => {
+const MenuItem = ({ title, items, updateCursorLocation }: TypeMenu) => {
   return (
     <>
-      <Button type="button" className={MenuItemStyle.container}>
+      <Button
+        type="button"
+        className={MenuItemStyle.container}
+        onMouseMove={(event) => {
+          if (!items || !updateCursorLocation) return;
+          updateCursorLocation(event);
+        }}
+      >
         <span className={MenuItemStyle.title}>{title}</span>
         {!!items && <Icon svg={IconArrow} />}
       </Button>
